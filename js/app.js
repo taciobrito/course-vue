e2852957577dae5c143e5e7dd691545f3fb77adb
taciobrito <tacio.7brito@gmail.com>
2017-11-28 15:50:54 -0200

    First Commit - Learning Framework Vue.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var hello = new Vue({
+	el: '#hello',
+	data: {
+		msg: "Hello Vue!",
+		people:[
+			{name: "Maria"},
+			{name: "Pedro"},
+			{name: "Gustavo"},
+			{name: "Joana"},
+			{name: "Gabriela"},
+		],
+	}
+});
