c8ea5ef33182718fc4614164024ffe1a2b422bc7
taciobrito <tacio.7brito@gmail.com>
2017-11-29 17:43:32 -0200

    buscando dados json

--- js/app.js
+++ js/app.js
@@ -1,50 +1,15 @@
-var hello = new Vue({
-	el: '#hello',
+var app = new Vue({
+	el: '#app',
 	data: {
-		msg: "Hello Vue!",
-		people:[
-			{name: "Maria"},
-			{name: "Pedro"},
-			{name: "Gustavo"},
-			{name: "Joana"},
-			{name: "Gabriela"},
-		],
-		newElement: '',
-		elements: [],
-		objectA:{
-			'font-size':'30px',
-		},
-		objectB:{
-			'color':'red',
-		},
-		myListForm: [],
-		myForm: {
-			name: '',
-			email: '',
-		},
+		books:[],
 	},
 	methods:{
-		addElement: function(){
-			var title = this.newElement.trim();
-			if( title ) {
-				this.elements.push({title: title});
-				this.newElement = '';
-			}
-		},
-		removeElement: function(e, index){
-			e.preventDefault();
-			this.elements.splice(index, 1);
-		},
-		myClick: function(){
-			alert('clicado');
-		},
-		myKeyup: function(){
-			alert('keypzado');
-		},
-		addForm: function(){
-			this.myListForm.push({name: this.myForm.name , email: this.myForm.email });
-			this.myForm.name = '';
-			this.myForm.email = '';
-		},
-	}
+		
+	},
+	created:function(){
+		var _self = this;
+		_self.$http.get('dataServer.json').then(function(response){
+			 _self.books = response.data;
+		});
+	},
 });
